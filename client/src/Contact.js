b7d0a03b2000f0b8262bbcca47c1a112534cb209
Akshat <akshat.agarwal11@gmail.com>
2023-03-09 10:11:51 +0530

    changes

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -1,37 +1,46 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Contact = () => {
   return (
-    <Wrapper>Contact</Wrapper>
-  )
-}
+    <Wrapper>
+      <h2>Contact</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15561.476714635652!2d80.03055548174288!3d12.819403956001059!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52f76c4db87a47%3A0x341fa471f8027d68!2sSRM%20University%2C%20Kattankulathur%2C%20Tamil%20Nadu%20603203!5e0!3m2!1sen!2sin!4v1678336533016!5m2!1sen!2sin"
+        width="100%"
+        height="400"
+        style={{border:0}}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"></iframe>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
-padding: 9rem 0 5rem 0;
-text-align: center;
-.container {
-  margin-top: 6rem;
-  .contact-form {
-    max-width: 50rem;
-    margin: auto;
-    .contact-inputs {
-      display: flex;
-      flex-direction: column;
-      gap: 3rem;
-      input[type="submit"] {
-        cursor: pointer;
-        transition: all 0.2s;
-        &:hover {
-          background-color: ${({ theme }) => theme.colors.white};
-          border: 1px solid ${({ theme }) => theme.colors.btn};
-          color: ${({ theme }) => theme.colors.btn};
-          transform: scale(0.9);
+  padding: 9rem 0 5rem 0;
+  text-align: center;
+  .container {
+    margin-top: 6rem;
+    .contact-form {
+      max-width: 50rem;
+      margin: auto;
+      .contact-inputs {
+        display: flex;
+        flex-direction: column;
+        gap: 3rem;
+        input[type="submit"] {
+          cursor: pointer;
+          transition: all 0.2s;
+          &:hover {
+            background-color: ${({ theme }) => theme.colors.white};
+            border: 1px solid ${({ theme }) => theme.colors.btn};
+            color: ${({ theme }) => theme.colors.btn};
+            transform: scale(0.9);
+          }
         }
       }
     }
   }
-}
 `;
 
-
-export default Contact
+export default Contact;
